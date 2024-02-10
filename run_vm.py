24e7338feb8bdcef367e2228003a35894f8bdd11
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-02-10 18:56:24 -0500

    tpm and docs

--- run_vm.py
+++ run_vm.py
@@ -152,6 +152,28 @@ def boot_vm():
       '-display', 'gtk,gl=on',
   ]
 
+  mytpm = '/tmp/mytpm1'
+  if os.path.exists(mytpm):
+    cmd += [
+      '-chardev', f'socket,id=chrtpm,path={mytpm}/swtpm-sock',
+      '-tpmdev', 'emulator,id=tpm0,chardev=chrtpm',
+      '-device', 'tpm-tis,tpmdev=tpm0'
+    ]
+  else:
+    print()
+    print('''
+Please run the following to emulate a TPM, if required.
+
+  mkdir /tmp/mytpm1
+  swtpm socket --tpmstate dir=/tmp/mytpm1 \
+    --ctrl type=unixio,path=/tmp/mytpm1/swtpm-sock \
+    --tpm2 \
+    --log level=20
+
+in another terminal.
+'''.strip())
+    print()
+
   print()
   print(f'>>> {" ".join(cmd)}')
   print()
